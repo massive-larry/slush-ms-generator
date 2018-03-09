87d741589198355dfcc4e76589b56c9405d1663e
larry eliemenye <larry.eliemenye@massive.co>
2018-03-09 15:56:16 +0000

    add some debug statements

--- transformers/project.js
+++ transformers/project.js
@@ -2,6 +2,8 @@ const through = require('through2');
 
 const packageConfigTransformer = function (options) {
     return through.obj(function (file, enc, cb) {
+        console.log('answers from input', options);
+        console.log('file recieved', file);
         cb();
     })
 };
